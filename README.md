a5e2a4c5b00924dbbdc21279a22dda0543a172bb
Funda Kartal <kartalfunda01@gmail.com>
2021-12-27 15:47:18 +0300

    Code all sections

---
+++ README.md
@@ -0,0 +1,58 @@
+# HTML+CSS Öğreniyoruz
+
+[TR](https://medium.com/adem-md/html-css-%C3%B6%C4%9Freniyoruz-b3c10f2441a8)
+
+[EN] In this [series of tutorials](https://www.youtube.com/playlist?list=PLadt0EaV4m3Ae9mBaQNylUKUaFK38F4EB), we gain basic HTML and CSS knowledge while coding the selected design.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Project Structure](#project-structure)
+  - [Content](#content)
+  - [Installation](#installation)
+  - [Screenshot](#screenshot)
+  - [Credits](#credits)
+
+## Overview
+
+### Project Structure
+
+css/main.css file is created from main.scss file which is inside the scss folder. main.scss file is used to import the other scss files. CSS codes of the components of the website are included in the scss/pages/index.scss. CSS codes of the header, navigation and footer are included in the scss/global folder. 
+
+PugJS is used as a template engine. Files of the template are in the views folder. Files in the views/component and views/global folder are extended from the file called master.pug. HTML codes of the common components of the website are included in the views/component which is in the views folder.
+
+### Content
+
+Topics covered in this tutorial:
+
+- Semantic HTML5 markup
+- CSS custom properties
+- The Basics of Object-Oriented CSS
+- Flexbox
+- Mobile-first workflow
+- Font-face and typography
+- Using SVG
+
+## Installation
+
+```bash
+git clone https://github.com/fundakartal/html-css-ogreniyoruz.git
+cd html-css-ogreniyoruz
+npm install or yarn install
+```
+
+## Start the server
+
+```bash
+gulp
+```
+
+Now enter [`localhost:3000`](http://localhost:3000) in the address bar of your browser.
+
+### Screenshot
+
+![screenshot](./images/ss-desktop.png)
+
+### Credits
+
+- Design: [Booking by Vladimir Gruev](https://dribbble.com/shots/3877462-Booking)
--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,46 @@
-const gulp = require('gulp')
-const browserSync = require('browser-sync').create()
-const sass = require('gulp-sass')
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const { src, dest } = require('gulp');
+const pug = require('gulp-pug');
 const prefix = require('gulp-autoprefixer')
-const plumber = require('gulp-plumber')
-const pug = require('gulp-pug')
-const reload = browserSync.reload
 
-gulp.task('browser-sync', function () {
+// compile scss into css
+function style() {
+  // 1. where is my scss file
+  return gulp.src('./scss/main.scss')
+  // 2. pass that file through sass compiler
+    .pipe(sass().on('error', sass.logError)) 
+  // prefix
+    .pipe(prefix())
+  // 3. where do I save the compiled CSS?
+    .pipe(gulp.dest('./css'))
+  // 4. stream changes to all browser
+    .pipe(browserSync.stream());
+}
+
+// compile pug file into html
+function htmlpug() {
+  return gulp.src('./views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./'))
+    .on('end', browserSync.reload)
+}
+
+function watch() {
   browserSync.init({
     notify: false,
     server: {
       baseDir: './'
     }
-  })
-  gulp.watch('./views/**/*.pug', ['html'])
-  gulp.watch('./scss/**/*.scss', ['css'])
-  gulp.watch('./js/**/*.js', reload)
-})
+  });
+  gulp.watch('./scss/**/*.scss', style)
+  gulp.watch('./views/**/*.pug', htmlpug)
+  gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
 
-gulp.task('css', () => {
-  return gulp.src('./scss/main.scss')
-  .pipe(plumber([{ errorHandler: false }]))
-  .pipe(sass())
-  .pipe(prefix())
-  .pipe(gulp.dest('./'))
-  .pipe(browserSync.stream())
-})
-
-gulp.task('html', () => {
-  return gulp.src('./views/*.pug')
-  .pipe(pug())
-  .pipe(gulp.dest('./'))
-  .on('end', reload)
-})
-
-gulp.task('default', ['browser-sync', 'html', 'css'])
+exports.style = style;
+exports.htmlpug = htmlpug;
+exports.watch = watch;
+exports.default = watch;
